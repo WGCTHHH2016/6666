1366928aad1db9680916feb409e26f80fdc80242
WGCTHHH2016 <1321496185@qq.com>
2016-07-02 14:56:18 +0800

    2016-2-1
    8888

---
+++ 6666/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  6666
+//
+//  Created by tarena1004 on 16/7/2.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 6666/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  6666
+//
+//  Created by tarena1004 on 16/7/2.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
